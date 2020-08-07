61c3ef029ffdc5987c576a8d8d0e045e098ed6b1
Pedro H. Breder Esteves <pedro.besteves@gmail.com>
2020-08-07 19:05:04 -0300

    style(): lint files

--- src/db/models/tokenModel.js
+++ src/db/models/tokenModel.js
@@ -5,7 +5,12 @@ const Schema = mongoose.Schema;
 const tokenSchema = new Schema({
   _userId: { type: mongoose.ObjectId, required: true, ref: "User" },
   token: { type: String, required: true },
-  createdAt: { type: Date, required: true, default: Date.now, expires: 43200 },
+  createdAt: {
+    type: Date,
+    required: true,
+    default: Date.now,
+    expires: 43200,
+  },
 });
 
 const Token = mongoose.model("Token", tokenSchema);
